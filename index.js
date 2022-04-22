1d7f04fc9e8a5638dbb7ec800282ec354bd3e4f7
Camilo Raitz da Silva <craitzus@gmail.com>
2022-04-22 10:35:40 -0300

    github action

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda Deployed by Camilos Home!'),
+        body: JSON.stringify('Hello from Lambda Deployed by Camilo Raitz Home!'),
     };
     return response;
 };
